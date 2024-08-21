02b3b7b950126b1f1b66cc60629d823deb196bec
Lhspr <88532882+Lhspr@users.noreply.github.com>
2024-08-21 15:05:39 +0700

    all

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
-import { CarouselHomepage } from '@/components/CarouselHomepage';
-
+import { CarouselHomepage } from '@/components/CarouselHomepage'; 
+import { Footer1 } from '@/components/Footer1';
+import { Berita } from '@/components/Berita';
 export default function HomePage() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Selamat Datang</h1>
+      {/* Carousel Section */}
       <CarouselHomepage />
+      {/* Card Section */}
+        <Berita />
+      {/* Footer Section */}
+      <Footer1 />
+        
     </div>
   );
 }
---
+++ components/Berita.jsx
@@ -0,0 +1,58 @@
+"use client";
+import { Card } from "flowbite-react";
+
+
+export function Berita() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
+      <Card className="max-w-sm" imgSrc="/images/blog/image-4.jpg" horizontal>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          berita 0
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+      </Card>
+      <Card className="max-w-sm" imgSrc="/images/blog/image-4.jpg" horizontal>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Berita 1
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+      </Card>
+      <Card className="max-w-sm" imgSrc="/images/blog/image-4.jpg" horizontal>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Berita 2
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+      </Card>
+      <Card className="max-w-sm" imgSrc="/images/blog/image-4.jpg" horizontal>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Berita 3
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+      </Card>
+      <Card className="max-w-sm" imgSrc="/images/blog/image-4.jpg" horizontal>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          berita 4
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+      </Card>
+      <Card className="max-w-sm" imgSrc="/images/blog/image-4.jpg" horizontal>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          berita 5
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        </p>
+      </Card>
+    </div>
+  );
+}

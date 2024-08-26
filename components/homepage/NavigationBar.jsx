fd0585ba8f663c91e27b7989fde031298076f594
Lhspr <88532882+Lhspr@users.noreply.github.com>
2024-08-26 07:45:34 +0700

    Update NavigationBar.jsx

--- components/homepage/NavigationBar.jsx
+++ components/homepage/NavigationBar.jsx
@@ -14,14 +14,14 @@ const NavigationBar = () => {
     })
   })
   return (
-    <Navbar fluid rounded className={`${isActive ? "bg-white py-4 shadow-md" : "bg-none py-6"
+    <Navbar fluid rounded className={`${isActive ? "bg-white shadow-md" : "bg-none"
       } fixed w-full z-10 transition-all`}>
       <Navbar.Brand as={Link} href="https://flowbite-react.com" className="flex justify-self-start">
         <Image
           height={55}
           width={200}
           src={icon}
-          className="ml-0 mr-3 sm:h-auto w-36 h-36" // Menghapus margin kiri untuk memastikan logo berada di kiri
+          className="ml-0 mr-3 sm:h-auto w-28 h-24" // Menghapus margin kiri untuk memastikan logo berada di kiri
           alt="Flowbite React Logo"
         />
       </Navbar.Brand>
@@ -31,18 +31,22 @@ const NavigationBar = () => {
           <Navbar.Link href="/" active>
             Beranda
           </Navbar.Link>
-          <Dropdown label='profil'inline>
-            <Dropdown.Item as='div'className='p-0'>
-              <Link className='block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600'href={'/struktur-organisasi'}>Struktur Organisasi</Link>
+          <Dropdown label='Profil' inline>
+            <Dropdown.Item as='div' className='p-0'>
+              <Link className='block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600' href={'/struktur-organisasi'}>Struktur Organisasi</Link>
             </Dropdown.Item>
           </Dropdown>
-          <Dropdown label='Berita'inline>
-            <Dropdown.Item as='div'className='p-0'>
-              <Link className='block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600'href={'/berita-semarang'}>Berita Semarang</Link>
+          <Dropdown label='Berita' inline>
+            <Dropdown.Item as='div' className='p-0'>
+              <Link className='block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600' href={'/berita-semarang'}>Berita Semarang</Link>
             </Dropdown.Item>
           </Dropdown>
-          <Navbar.Link href="#">PPID</Navbar.Link>
-          <Navbar.Link href="#">Berita</Navbar.Link>
+          <Navbar.Link href="LINK">PPID</Navbar.Link>
+          <Dropdown label='Gallery' inline>
+            <Dropdown.Item as='div' className='p-0'>
+              <Link className='block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600' href={'/berita-semarang'}>Berita Semarang</Link>
+            </Dropdown.Item>
+          </Dropdown>
           <Navbar.Link href="#">Contact</Navbar.Link>
         </Navbar.Collapse>
       </div>

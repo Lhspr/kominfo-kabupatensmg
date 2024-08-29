6932fa33a1a8d157144046fbe137ab108541a762
Lhspr <88532882+Lhspr@users.noreply.github.com>
2024-08-29 10:12:30 +0700

    Update CarouselHomepage.jsx

--- components/CarouselHomepage.jsx
+++ components/CarouselHomepage.jsx
@@ -13,31 +13,31 @@ export function CarouselHomepage() {
       {/* Main Container */}
       <div className="flex w-full max-w-screen-xl">
         {/* Sidebar */}
-        <div className="w-1/4 p-4 bg-white shadow-md rounded-lg"> {/* Changed background to white */}
+        <div className="w-1/4 p-4 bg-white shadow-md rounded-lg">
           <ul className="space-y-2">
             <li className="flex items-center p-4 rounded-lg bg-white hover:bg-[#007bff] transition dark:bg-[#f0f0f0] dark:hover:bg-[#007bff]">
-              <FaPaperPlane className="text-[#007bff] mr-3" size={24} /> {/* Changed icon color to blue */}
+              <FaPaperPlane className="text-[#007bff] mr-3" size={24} />
               <div>
                 <div className="text-gray-900 dark:text-white font-bold">TERBARU</div>
                 <div className="text-gray-500 dark:text-gray-400 text-sm">Rangkuman Berita</div>
               </div>
             </li>
             <li className="flex items-center p-4 rounded-lg bg-white hover:bg-[#007bff] transition dark:bg-[#f0f0f0] dark:hover:bg-[#007bff]">
-              <FaMicrophone className="text-[#007bff] mr-3" size={24} /> {/* Changed icon color to blue */}
+              <FaMicrophone className="text-[#007bff] mr-3" size={24} />
               <div>
                 <div className="text-gray-900 dark:text-white font-bold">SIARAN PERS</div>
                 <div className="text-gray-500 dark:text-gray-400 text-sm">Siaran Pers Terbaru</div>
               </div>
             </li>
             <li className="flex items-center p-4 rounded-lg bg-white hover:bg-[#007bff] transition dark:bg-[#f0f0f0] dark:hover:bg-[#007bff]">
-              <FaBullhorn className="text-[#007bff] mr-3" size={24} /> {/* Changed icon color to blue */}
+              <FaBullhorn className="text-[#007bff] mr-3" size={24} /> 
               <div>
                 <div className="text-gray-900 dark:text-white font-bold">BERITA</div>
                 <div className="text-gray-500 dark:text-gray-400 text-sm">Berita Terbaru</div>
               </div>
             </li>
             <li className="flex items-center p-4 rounded-lg bg-white hover:bg-[#007bff] transition dark:bg-[#f0f0f0] dark:hover:bg-[#007bff]">
-              <FaBullhorn className="text-[#007bff] mr-3" size={24} /> {/* Changed icon color to blue */}
+              <FaBullhorn className="text-[#007bff] mr-3" size={24} /> 
               <div>
                 <div className="text-gray-900 dark:text-white font-bold">PENGUMUMAN</div>
                 <div className="text-gray-500 dark:text-gray-400 text-sm">Pengumuman Terbaru</div>

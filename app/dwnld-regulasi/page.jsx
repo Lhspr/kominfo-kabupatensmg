08891ee674a2db4241591ed5ab02987025be6cde
ariaardiansyah37 <92778349+ariaardiansyah37@users.noreply.github.com>
2024-08-29 09:44:14 +0700

    regulasi

---
+++ app/dwnld-regulasi/page.jsx
@@ -0,0 +1,10 @@
+
+import { Regulasi } from '@/components/dwnld-regulasi/Regulasi';
+
+export default function RegulasiPage() {
+  return (
+    <div className="container mx-auto">
+      <Regulasi/>
+    </div>
+  );
+}

2cbf9e45b234f161a7081faad5d60f033ff809ff
Lhspr <88532882+Lhspr@users.noreply.github.com>
2024-09-12 23:43:49 +0700

    app

--- components/Gallery-foto/Foto.jsx
+++ components/Gallery-foto/Foto.jsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Card, Pagination } from 'flowbite-react';
 import gambar2 from '@/assets/kegiatan1.jpeg';
 import gambar3 from '@/assets/kegiatan2.jpeg';
@@ -9,32 +10,48 @@ import Image from 'next/image';
 export function Foto() {
   // State untuk mengelola pagination
   const [currentPage, setCurrentPage] = useState(1);
+  const [photos, setPhotos] = useState([])
   const photosPerPage = 9; // Jumlah foto per halaman
 
-  const onPageChange = (page) => {
-    setCurrentPage(page);
-  };
 
-  // Array semua foto yang ingin ditampilkan
-  const allPhotos = [
-    gambar2, gambar3, gambar2, gambar3, gambar2, gambar3, gambar2, gambar3, gambar2,
-    gambar3, gambar2, gambar3, gambar2, gambar3, gambar2, gambar3, gambar2, gambar3,
-    gambar2, gambar3, gambar2, gambar3, gambar2, gambar3, gambar2, gambar3, gambar2,
-  ]; // Total ada 27 foto
+  const getdatafoto = async () => {
+    axios
+      .get("http://127.0.0.1:8000/api/v1/photos", {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then((response) => {
+        const data = response.data.data;
+        setPhotos(data);
+      })
+      .catch((error) => {
+        alert(`Failed to get data photo to server [${error}]`)
+      });
+  }
 
-  const totalPages = Math.ceil(allPhotos.length / photosPerPage);
+    const onPageChange = (page) => {
+      setCurrentPage(page);
+    };
+
+  const totalPages = Math.ceil(photos.length / photosPerPage);
 
   // Menentukan foto yang akan ditampilkan di halaman saat ini
-  const currentPhotos = allPhotos.slice(
+  const currentPhotos = photos.slice(
     (currentPage - 1) * photosPerPage,
     currentPage * photosPerPage
   );
+
+  useEffect(() => {
+    getdatafoto()
+  },[0]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-40 px-5">
       <h1 className="text-3xl font-bold mb-4">GALERI TERBARU</h1>
       <h2 className="text-lg mb-10">
-        Informasi terbaru dari DISKOMINFO Kabupaten Semarang dalam bentuk Foto & Video
+        Informasi terbaru dari DISKOMINFO Kabupaten Semarang dalam bentuk Foto &
+        Video
       </h2>
 
       {/* Konten Utama untuk Galeri Foto */}
@@ -44,17 +61,17 @@ export function Foto() {
           <div className="flex flex-col items-center justify-center w-full lg:w-3/4">
             {/* Grid Galeri */}
             <div className="grid grid-cols-3 gap-9">
-              {currentPhotos.map((photo, index) => (
+                {photos.map((item, index) => (
                 <div key={index} className="flex justify-center">
                   <Card className="w-full max-w-xs">
-                    <Image
-                      src={photo}
+                    <img
+                      src={item.file_path}
                       alt={`Kegiatan Diskominfo ${index + 1}`}
                       className="w-full h-auto"
                     />
-                  </Card>
-                </div>
-              ))}
+                  </Card> 
+                 </div>
+              ))} 
             </div>
           </div>
         </div>
--- components/berita-semarang/BeritaSemarang.jsx
+++ components/berita-semarang/BeritaSemarang.jsx
@@ -1,10 +1,32 @@
 'use client';
 
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Card } from 'flowbite-react';
-import gambar3 from '@/assets/berita1.png';
-import Image from 'next/image';
 
 export function BeritaSemarang() {
+  const [news,setNews] = useState([])
+
+  const getData = async () => {
+    axios
+      .get("http://127.0.0.1:8000/api/v1/news", {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then((response) => {
+        const data = response.data.data;
+        console.log(data);
+        setNews(data);
+      })
+      .catch((error) => {
+        alert(`Failed to get data news to server [${error}]`);
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [0]);
   return (
     <div className="flex flex-col items-center pt-20 mb-32">
       {/* Heading di atas card */}
@@ -19,24 +41,26 @@ export function BeritaSemarang() {
       <div className="flex flex-col items-center justify-center w-full max-w-screen-lg">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pt-10 w-full max-w-screen-lg">
           {/* Berita Cards */}
-          {[...Array(6)].map((_, index) => (
-            <Card key={index} className="shadow-xl flex flex-col items-center justify-center">
+          {news.map((item, index) => (
+            <Card
+              key={index}
+              className="shadow-xl flex flex-col items-center justify-center"
+            >
               {/* Image */}
-              <Image
+              <img
                 width={400}
                 height={250}
-                src={gambar3}
+                src={item.image}
                 alt="image"
                 className="w-full h-64 object-cover"
               />
               {/* Content */}
               <div className="p-4 text-center">
                 <h5 className="text-lg font-semibold tracking-tight text-gray-900 dark:text-white">
-                  Noteworthy technology acquisitions 2021
+                  {item.title}
                 </h5>
                 <p className="text-sm font-light text-gray-700 dark:text-gray-400 mb-4">
-                  Here are the biggest enterprise technology acquisitions of
-                  2021 so far, in reverse chronological order.
+                  {item.description}
                 </p>
                 <a
                   href="#"
